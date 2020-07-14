8c03448cb45db18d2527751e83deacc2a1a519e4
Chris Pfaff <chrispfaff10@gmail.com>
2020-07-13 20:54:31 -0500

    updated app

--- src/Components/App/App.svelte
+++ src/Components/App/App.svelte
@@ -1,21 +1,23 @@
 <script>
-	import News from "./News.svelte"
-	import Header from "./Header.svelte"
+  import News from "./News.svelte";
+  import Header from "./Header.svelte";
+  import Sidebar from "./Sidebar.svelte";
 </script>
 
-<main>
-	<Header/>
-	<News/>
-</main>
-
 <style>
-	main {
-		background-color: #03B5AA;	
-	}
+  main {
+    background-color: #03b5aa;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <Header />
+  <Sidebar />
+  <News />
+</main>
