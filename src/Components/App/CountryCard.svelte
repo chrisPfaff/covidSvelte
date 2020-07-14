f625d1a7e74960a079e90540c486094eadba52fa
Chris Pfaff <chrispfaff10@gmail.com>
2020-07-13 20:54:57 -0500

    fixed country card

--- src/Components/App/CountryCard.svelte
+++ src/Components/App/CountryCard.svelte
@@ -1,21 +1,36 @@
 <script>
-    export let country;
-    export let newConfirmed;
-    export let totalConfirmed;
+  export let country;
+  export let continent;
+  export let flag;
+  export let active;
+  export let deaths;
 </script>
 
-<div>
-    <h2>{country}</h2>
-    <p>New Cases: {newConfirmed}</p>
-    <p>Total Cases: {totalConfirmed}</p>
-</div>
-
 <style>
-    div {
-        background-color: #037971;
-        width: 400px;
-        margin: 20px;
-        padding: 20px;
-    }
+  @import url("https://fonts.googleapis.com/css2?family=Lato&display=swap");
+
+  div {
+    background-color: #333333;
+    color: #95b8d1;
+    width: 400px;
+    margin: 20px;
+    padding: 20px;
+    border-radius: 1em;
+  }
+  h2 {
+  }
+  p {
+    font-family: "lato", sans-serif;
+    padding-top: 10px;
+  }
 </style>
 
+<div>
+  <h2>
+    <img src={flag} alt="flag of" />
+  </h2>
+  <p>{country}</p>
+  <p>{continent}</p>
+  <p>Active Cases: {active}</p>
+  <p>Deaths: {deaths}</p>
+</div>
