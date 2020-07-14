23d3e3d1b200d9364de5e573a72ae451a0393863
Chris Pfaff <chrispfaff10@gmail.com>
2020-07-13 20:55:25 -0500

    fixed header

--- src/Components/App/Header.svelte
+++ src/Components/App/Header.svelte
@@ -1,23 +1,28 @@
 <script>
+
 </script>
 
-<header>
-    <div class="links">
-        <a href="#">Home</a>
-        <a href="#">Login</a>
-    </div>    
-</header>
-
 <style>
-    .links {
-        background-color: #023436;
-        display: flex;
-        height: 3em;
-    }
-    a {
-        text-decoration: none;
-        font-size: 2em;
-        color: white;
-    }
+  header {
+    position: fixed;
+    top: 0;
+    z-index: 2;
+    width: 100%;
+  }
+  .links {
+    background-color: #333333;
+    display: flex;
+  }
+  a {
+    text-decoration: none;
+    font-size: 2em;
+    color: white;
+  }
 </style>
 
+<header>
+  <div class="links">
+    <a href="#">Home</a>
+    <a href="#">Login</a>
+  </div>
+</header>
